a7be08f930712849b6bf0b7700e51f15df2a1448
chobobdev <chobobdev@naver.com>
2024-03-19 02:27:35 +0900

    init: initialize the kotlin project

---
+++ src/main/kotlin/com/chobobdev/kotleaf/KotleafApplication.kt
@@ -0,0 +1,11 @@
+package com.chobobdev.kotleaf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotleafApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotleafApplication>(*args)
+}
